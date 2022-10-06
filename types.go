6c42a016f83c6aa95c72a573cb37d4e9b8500259
mariownyou <smj510black@gmail.com>
2022-10-06 12:04:30 +0200

    small changes

--- types.go
+++ types.go
@@ -5,33 +5,29 @@ import (
 	"fmt"
 )
 
-
 type Place struct {
-	Center	  []float32 `json:"center"`
+	Center    []float32 `json:"center"`
 	PlaceName string    `json:"place_name"`
 	Relevance float32   `json:"relevance"`
 	Address   string    `json:"address"`
 }
 
-
 type Response struct {
 	Features []Place `json:"features"`
 }
 
-
 type CityCoordinates struct {
-	Lat		float32
-	Lng		float32
-	Name	string
+	Lat     float32
+	Lng     float32
+	Name    string
 	Address string
 }
 
-
-func (c *CityCoordinates) UnmarshalJSON(data []byte) error {	
+func (c *CityCoordinates) UnmarshalJSON(data []byte) error {
 	var vals Response
-    if err := json.Unmarshal(data, &vals); err != nil {
-        return err
-    }
+	if err := json.Unmarshal(data, &vals); err != nil {
+		return err
+	}
 
 	if len(vals.Features) > 0 {
 		place := vals.Features[0]
@@ -40,10 +36,9 @@ func (c *CityCoordinates) UnmarshalJSON(data []byte) error {
 		c.Lat = place.Center[1]
 		c.Address = place.Address
 	}
-	
+
 	return nil
 }
-
 
 func (c *CityCoordinates) String() string {
 	if c.Name == "" {
@@ -51,4 +46,3 @@ func (c *CityCoordinates) String() string {
 	}
 	return fmt.Sprintf("%s", c.Name)
 }
-
